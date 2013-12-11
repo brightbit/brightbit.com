a5b15cafb01f75fce124057f141bb0b527af5008
Joshua <joshua@joshuaogle.com>
2013-12-11 00:14:16 -0600

    Multiple blogs and nested layouts

--- config.rb
+++ config.rb
@@ -1,38 +1,14 @@
 Time.zone = "UTC"
 
-activate :blog do |blog|
-  blog.layout = "layouts/blog"
-  blog.permalink = "blog-:year-:month-:day-:title.html"
-  blog.taglink = "tags/:tag.html"
-  blog.tag_template = "tag.html"
-  blog.calendar_template = "calendar.html"
-  # blog.summary_separator = /(READMORE)/
-  # blog.summary_length = 250
-  # blog.paginate = true
-  # blog.per_page = 5
-  # blog.page_link = "page/:num"
-end
-
 page "/feed.xml", :layout => false
-page "blog/*", :layout => :blog
-
-# page "/path/to/file.html", :layout => false
-# page "/path/to/file.html", :layout => :otherlayout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
 
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
 
 activate :directory_indexes
+
+
 
 configure :build do
   # activate :minify_css
@@ -41,4 +17,28 @@ configure :build do
   # activate :relative_assets
   require "middleman-smusher"
   activate :smusher
+end
+
+
+
+# Multiple Blogs
+activate :blog do |blog|
+  blog.name              = "news"
+  blog.layout            = "post"
+  blog.permalink         = ":year-:month-:day-:title.html"
+  blog.prefix            = "news"
+  blog.tag_template      = "tag.html"
+  blog.taglink           = "tags/:tag.html"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length    = 250
+  # blog.per_page          = 5
+  # blog.page_link         = "page/:num"
+end
+
+activate :blog do |blog|
+  blog.name              = "articles"
+  blog.layout            = "post"
+  blog.permalink         = ":year-:month-:day-:title.html"
+  blog.prefix            = "articles"
+  blog.tag_template      = "tag.html"
 end
