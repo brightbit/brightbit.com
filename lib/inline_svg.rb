5a69570a179856962fe0731094a2f13eaa378a87
Joshua <joshua@joshuaogle.com>
2013-12-19 16:56:49 -0600

    Site updated to e9e87d2

--- lib/inline_svg.rb
+++
@@ -1,8 +0,0 @@
-module InlineSVG
-
-  def inline_svg(path)
-    file = File.open("source/images/#{path}", "rb")
-    file.read
-  end
-
-end
--- source/javascripts/application.js
+++
@@ -1,5 +0,0 @@
-//= require modernizr
-//= require jquery
-//= require affix
-//= require alert
-//= require global
