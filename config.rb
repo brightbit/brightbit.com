db0319f4c73f826ac4871507947faee8a0519116
Eric Boehs <ericboehs@gmail.com>
2014-01-16 01:52:15 -0600

    Use Last-Modified & asset_hash (not cache_buster)
     * Use my patch for Rack::StaticCache which a) fixes files ending with a
       dash followed by a number and b) doesn't unnecessarily delete
       Last-Modified headers (pending PR open)
     * Use middleman's asset_hash extension rather than cache_buster;
       cache_buster appended a query string which isn't always honored by
       proxies
     * Re-enable Rack::StaticCache

--- config.rb
+++ config.rb
@@ -17,7 +17,7 @@ helpers InlineSVG
 configure :build do
   activate :minify_css
   activate :minify_javascript
-  activate :cache_buster
+  activate :asset_hash
   # activate :relative_assets
 end
 
